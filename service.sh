b517795fa9929e9e040527627f18ef71d5a0df16
XelXen <xelte.xynos@gmail.com>
2021-12-19 01:04:45 +0530

    Prettify scripts

--- service.sh
+++ service.sh
@@ -1,30 +1,37 @@
 #!/system/bin/sh
-# XEngine™ | Service Script
+# XEngine™ Service Script
 
 modpath="/data/adb/modules/xengine/"
 
 wait_until_login(){
   # In case of /data encryption is disabled
+
   while [[ "$(getprop sys.boot_completed)" != "1" ]]; do
       sleep 1
   done
 
   # We don't have the permission to rw "/storage/emulated/0" before the user unlocks the screen
+
   local test_file="/storage/emulated/0/Android/.PERMISSION_TEST"
   true > "$test_file"
+
   while [[ ! -f "$test_file" ]]; do
     true > "$test_file"
     sleep 1
+
   done
   rm "$test_file"
+
 }
 
+# Execute Login function
 wait_until_login
 
 # Sleep some time to complete init
 sleep 60
 
-# Initialize engine
+# Initialize Engine
+
 "${modpath}engine/xengine1" 2>/dev/null
 ("${modpath}engine/xengine2" &)& 2>/dev/null
 ("${modpath}engine/xengine3" &)& 2>/dev/null
--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/sbin/sh
+
 ###########################
-# MMT Bourne Setup Script
+# MMT Bourne Base Script
+# XEngine™️ Modifications by @XelXen and @iamlooper
 ###########################
 
 ############
@@ -11,24 +13,29 @@
 # Uncomment if you want to clean old files in module before injecting new module
 # Uncomment if you want to load vskel after module info print. If you want to manually load it, consider using load_vksel function
 # Uncomment DEBUG if you want full debug logs (Saved To Internal Storage)
-#SKIPMOUNT=true
+
+# SKIPMOUNT=true
 CLEANSERVICE=true
-#AUTOVKSEL=true
+# AUTOVKSEL=true
 DEBUG=true
 
 ############
 # Replace List
 ############
 
-# List all directories you want to directly replace in the system
-# Construct your list in the following example format
+
+## List all directories you want to directly replace in the system
+## Construct your list in the following example format
+
 REPLACE_EXAMPLE="
 /system/app/Youtube
 /system/priv-app/SystemUI
 /system/priv-app/Settings
 /system/framework
 "
-# Construct your own list here
+
+## Construct your own list here
+
 REPLACE="
 "
 
@@ -36,21 +43,25 @@ REPLACE="
 # Cleanup
 ############
 
-# Remove unnecessary stuff
+
+## Remove unnecessary stuff
+
 do_cleanup(){
   rm -rf "$MODPATH/LICENSE" 2>/dev/null
   rm -rf "$MODPATH/README.md" 2>/dev/null
   rm -rf "$MODPATH/xengine.png" 2>/dev/null
   rm -rf "$MODPATH/XToast.apk" 2>/dev/null
-  rm -rf "$MODPATH/xengine64.tar.xz"
-  rm -rf "$MODPATH/xengine32.tar.xz"
+  rm -rf "$MODPATH/xengine64.tar.xz" 2>/dev/null
+  rm -rf "$MODPATH/xengine32.tar.xz" 2>/dev/null
 }
 
 ############
 # Permissions
 ############
 
-# Set permissions
+
+## Set permissions
+
 set_permissions(){
   set_perm_recursive "$MODPATH" 0 0 0777 0755
   set_perm_recursive "$MODPATH/engine" 0 0 0777 0755
@@ -61,11 +72,13 @@ set_permissions(){
 # Info Print
 ############
 
-# Set what you want to be displayed on header of installation process
+
+## Set what you want to be displayed on header of installation process
+
 info_print(){
   awk '{print}' "$MODPATH/xengine_banner"
   ui_print ""
-  ui_print "[⚡] ENERGY-AWARE ANDROID OPTIMIZATION AI ENGINE [⚡]"
+  ui_print "[⚡] ENERGY-AWARE OPTIMIZATION ENGINE BASED ON PAI [⚡]"
   ui_print ""
   sleep 1.5
 }
@@ -74,36 +87,49 @@ info_print(){
 # Main
 ############
 
-# Change the logic to whatever you want
+## Change the logic to whatever you want
+
 int_main(){
+
   ui_print "[*] Injecting engine into system... "
   ui_print ""
+
   if [[ "$IS64BIT" == "true" ]]; then
     tar -xf "$MODPATH/xengine64.tar.xz" -C "$MODPATH"
   else
     tar -xf "$MODPATH/xengine32.tar.xz" -C "$MODPATH"
   fi 
-  pm install "$MODPATH/XToast.apk"
-  sleep 1.5
-  ui_print "[*] Setting up executable permissions... "
-  sleep 1
+
+  ui_print "[*] Installing Toast API for XEngine... "
   ui_print ""
+  pm install "$MODPATH/XToast.apk"
+  sleep 1
+
   ui_print "[*] Cleaning up... "
   ui_print ""
+
+  if [[ "$IS64BIT" == "true" ]]; then
+    rm -rf "$MODPATH/xengine32.tar.xz"
+  else
+    rm -rf "$MODPATH/xengine64.tar.xz"
+  fi 
+
   sleep 1
   ui_print "[*] Done!"
   ui_print ""
-  sleep 1.5
+  sleep 1
+
   ui_print " --- Notes --- "
   ui_print ""
-  ui_print "[*] Reboot is required"
+  ui_print "[🔁] Reboot is required"
   ui_print ""
-  ui_print "[*] Do not use XEngine™ with other optimizer modules"
+  ui_print "[⛔] Do not use XEngine™ with other optimizer modules"
   ui_print ""
-  ui_print "[*] (su -c xmenu) to open XMenu™ in Termux"
+  ui_print "[👉🏻] (su -c xmenu) to open XMenu™ in Termux"
   ui_print ""
-  ui_print "[*] Report issues to @tweak_projects_discuss on Telegram"
+  ui_print "[⚠️] Report issues to @tweak_projects_discuss on Telegram"
   ui_print ""
-  ui_print "[*] You can find me at iamlooper @ Telegram for direct support"
+  ui_print "[💬] You can find me at iamlooper @ Telegram for direct support"
   sleep 4
+
 }
